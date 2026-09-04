aaa367a37fd38badf81b979539c08cb7650dd4c7
Doriane CHETTIBI <dchet@lmdsio.com>
2026-09-04 14:44:13 +0200

    Mon projet version 2

---
+++ acceuil.php
@@ -0,0 +1,17 @@
+<?php
+$login= $_POST['login'];
+$mdp= $_POST['password'];
+
+echo $login;
+echo $mdp;
+
+$host= $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+
+if($login=="admin" && $mdp=="azerty"){
+    header("Location: http://$host$uri/profil.html");
+}else{
+    header("Location: http://$host$uri/index.html");
+}
+
+

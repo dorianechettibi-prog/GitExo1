8bc68bd189d7e939147b4a58e6fa0e5f3c747369
Doriane CHETTIBI <dchet@lmdsio.com>
2026-09-04 14:48:06 +0200

    Mon projet version 3

--- acceuil.php
+++ acceuil.php
@@ -9,9 +9,10 @@ $host= $_SERVER['HTTP_HOST'];
 $uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 
 if($login=="admin" && $mdp=="azerty"){
-    header("Location: http://$host$uri/profil.html");
-}else{
     header("Location: http://$host$uri/index.html");
+}else{
+
+    header("Location: http://$host$uri/profil.html");
 }
 
 

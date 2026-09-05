e1a2e0ac40b4e371e91880023d02f64975f1bbe2
Prenom Nom <nom.prenom@lmdsio.fr>
2026-09-05 18:14:51 +0200

    Mon projet version 2

--- Git-exo1/acceuil.php
+++ Git-exo1/acceuil.php
@@ -1,5 +1,14 @@
 <?php
-$prenom = $_POST['prenom'];
-$nom = $_POST['nom'];
-echo "bonjour $prenom, $nom ";
+$login=$_POST['login'];
+$mdp = $_POST['mdp'];
+
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+
+if ($login == "admin" && $mdp =="azerty"){
+    header("Location: http://$host$uri/profil.html");
+} else {
+    header("Location: http://$host$uri/index.html");
+}
 ?>
